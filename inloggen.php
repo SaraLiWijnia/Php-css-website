6d40f098130c3d18b8eca24a975591228cd9721d
SaraLiWijnia <saraliwynia@gmail.com>
2019-01-26 22:45:24 +0100

    includes head, footer
    start css

---
+++ inloggen.php
@@ -0,0 +1,30 @@
+<?php include 'includes\head.html'; ?>
+
+<body>
+<nav class="mainnav">
+<ol> 
+<li> home </li>   
+  <li> blog </li>
+  <li> forum </li> 
+  <li> log in  </li>
+  <li> drop down menu 
+    <ol> 
+      <li> optie 1 </li>
+      <li> optie 2 </li> 
+      <li> optie 3 </li> 
+    </ol>
+  </li>
+</ol>
+</nav>
+
+<main>
+
+</main>
+
+
+</body>
+
+<?php include 'includes\footer.html'; ?>
+
+
+</html>
